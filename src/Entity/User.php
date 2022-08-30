cdc72fafa7e0009d3bacfad57515b8eaba353d58
Tsunaa-Alexis <alexisottavi@gmail.com>
2022-08-30 11:27:40 +0200

    register + login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,15 +2,18 @@
 
 namespace App\Entity;
 
+use App\Entity\Order;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['mail'], message: 'There is already an account with this mail')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
