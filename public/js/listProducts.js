098d337468adf072d4f826cf5c20af8cd2f874f5
pauline <pauline.gidon@mezcalito.fr>
2022-08-31 14:15:31 +0200

    basket in local storage

--- public/js/listProducts.js
+++ public/js/listProducts.js
@@ -3,8 +3,10 @@ function reRender(products)
 {
     productContainer.innerHTML = '';
     products.forEach(product => {
+        let str = JSON.stringify(product);
         let productElement = document.createElement('div');
         productElement.classList.add('card','card-list-product');
+
         productElement.innerHTML = `
             
             <div class="card-body">
@@ -12,10 +14,14 @@ function reRender(products)
                 <img src="uploads/pictures/products/${product.pictures}" class="full-img"/>
                 <p class="card-text">${product.description}</p>
                 <p>${product.price} €</p>
-                <a href="#" class="btn btn-primary">Ajouter à ma commande</a>
+                <p class="btn btn-primary add-order" data-product-name="${product.name}" data-product-price="${product.price}">Ajouter à ma commande</p>
             </div>
         `;
         productContainer.append(productElement);
+        let btns = document.querySelectorAll('.add-order')
+        for(let i = 0; i < btns.length; i++ ){
+            btns[i].addEventListener('click', addMyOrder)
+        }
     })
 }
 
@@ -25,6 +31,59 @@ async function fetchProductsByCategory()
      await fetch(url, {method : 'GET'})
         .then(response => response.json())
         .then(result => reRender(result)).catch(error => console.log(error));
+}
+
+function addMyOrder(){
+
+    let productOrder = {
+        "name": this.dataset.productName,
+        "price": this.dataset.productPrice,
+    }
+    addProductBasket(productOrder);
+}
+function saveBasket(basket){
+    localStorage.setItem("basket", JSON.stringify(basket))
+}
+
+function getBasket(){
+    let basket= localStorage.getItem("basket");
+    if(null == basket){
+        return [];
+    }else {
+        return JSON.parse(basket);
+    }
+}
+
+function addProductBasket(product){
+    let basket = getBasket();
+    let foundProduct = basket.find(p => p.name == product.name);
+    console.log(foundProduct);
+    if(foundProduct != undefined)
+    {
+        foundProduct.quantity++;
+    }else{
+        product.quantity = 1;
+        basket.push(product);
+    }
+    saveBasket(basket);
+}
+function removeProductBasket(product){
+    let basket = getBasket();
+    basket = basket.filter(p => p.name != product.name);
+    saveBasket(basket);
+}
+function changeQuantity(product, quantity){
+    let basket = getBasket();
+    let foundProduct = basket.find(p => p.name == product.name);
+    if(foundProduct != undefined)
+    {
+        foundProduct.quantity += quantity;
+        if(foundProduct.quantity <= 0 ){
+            removeProductBasket(foundProduct);
+        }else{
+            saveBasket(basket);
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
